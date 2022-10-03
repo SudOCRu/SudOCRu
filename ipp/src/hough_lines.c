752c09099ae494db61ed26a89eeb07ae8b67e078
Sofiane <sofiane.meftah@outlook.fr>
2022-10-03 23:13:01 +0200

    feat: Hough Lines function outlines

---
+++ ipp/src/hough_lines.c
@@ -0,0 +1,13 @@
+#include "hough_lines.h"
+
+Line** HoughLines(const Image* image, int* found_count)
+{
+    // TODO
+    *found_count = 0;
+    return NULL;
+}
+
+void RenderLines(Image* image, unsigned int color, Line** lines, int len)
+{
+    // TODO
+}
