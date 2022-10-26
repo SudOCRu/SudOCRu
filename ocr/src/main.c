8eb56194b7af9c673dc48e394898036247e514b3
mehdi.el-alaoui-el-aoufoussi <mehdi.el-alaoui-el-aoufoussi@epita.fr>
2022-10-26 16:09:49 +0200

    Added arguments
    Signed-off-by: mehdi.el-alaoui-el-aoufoussi <mehdi.el-alaoui-el-aoufoussi@epita.fr>

--- ocr/src/main.c
+++ ocr/src/main.c
@@ -2,6 +2,9 @@
 #include <stdlib.h>
 #include <err.h>
 #include <time.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <errno.h>
 #include "datapoint.h"
 #include "neuralnetwork.h"
 #include "train.h"
@@ -92,11 +95,106 @@ int GetMaxIndex(double* data, int size){
     return sample;
 }*/
 
-void ReadNetwork(NeuralNetwork *network);
+int TrainCmd(int argc, char **argv){
+    NeuralNetwork *network = NULL;
+    int layerStructure[] = {2, 2, 2};
+    long learnTimes;
+    double learnRate;
+    char *filename = NULL;
+    if (argc >= 4){
+        char *iCount;
+        char *lRate;
+        if (strcmp(argv[2], "-f") == 0){
+            if (argc < 4){
+                printf("Please specify file name\n.");
+                return 1;
+            }
+            filename = argv[3];
+            struct stat buffer;
+            if (stat(filename, &buffer) == 0){
+                network = ReadNetwork(filename);
+            } else {
+                printf("Neural network file doesn't exist. Creating a new one.\n");
+            }
+            if (argc != 6){
+                printf("You have to specify learn iterations and learn rate.\n");
+                return 1;
+            }
+            iCount = argv[4];
+            lRate = argv[5];
+        } else {
+            iCount = argv[2];
+            lRate = argv[3];
+        }
+        learnTimes = strtol(iCount, NULL, 10);
+        learnRate = strtod(lRate, NULL);
+        if (learnTimes == 0L || learnRate == 0 || errno == ERANGE){
+            printf("Wrong argument: %s\n",iCount);
+            return 1;
+        }
+    } else {
+        printf("Not enought arguments !\n");
+        return 1;
+    } 
 
-int main()
+    if (network == NULL){
+        srand(time(NULL));
+        network = CreateNeuralNetwork(layerStructure, 3);
+    }
+    
+    DataPoint **sample = GenerateXorData();
+    for (long i = 0; i < learnTimes; i++){
+        Learn(network, sample, 4, learnRate);
+    }
+    SaveNetwork(network, (filename == NULL) ? "network" : filename);
+    printf("Neural network saved in:\"%s\"\n",(filename == NULL) ? "network" : filename);
+    
+
+    DestroyNeuralNetwork(network);
+    for (int i = 0; i < 4; i++){
+        DestroyDatapoint(sample[i]);
+    }
+    free(sample);
+    return 0;
+}
+
+int EvalCmd(int argc, char **argv){
+    char *file = argv[2];
+    char *input = argv[3];
+    NeuralNetwork *network = ReadNetwork(file);
+    double *inputs = calloc(network->layers[0]->numNodesIn, sizeof(double));
+    char *token = strtok(input, ",");
+    for (int i = 0; token != NULL; i++){
+        inputs[i] = atoi(token);
+        token = strtok(NULL, ",");
+    }
+
+    double *output = ProcessOutputs(network, inputs);
+    for (int i = 0; i < network->layers[network->arrayLayerLength-1]->numNodesOut; i++){
+        printf("Output%i = %lf\n", i, output[i]);
+    }
+
+    DestroyNeuralNetwork(network);
+    free(output);
+    return 0;
+}
+
+int main(int argc, char *argv[])
 {
-    srand(time(NULL));
+    if (argc == 1){
+        printf("Please use an argument.\n");
+        return 1;
+    }
+
+    if (strcmp(argv[1], "train") == 0){
+        return TrainCmd(argc, argv);
+    } else if (argc == 4 && strcmp(argv[1], "eval") == 0){
+        return EvalCmd(argc, argv);
+    } else {
+        printf("Not enough arguments.\n");
+        return 1;
+    }
+    /*srand(time(NULL));
     int layerStructure[] = {2, 2, 2};
     DataPoint **sample = GenerateXorData();
     NeuralNetwork *network = CreateNeuralNetwork(layerStructure, 3);
@@ -104,16 +202,15 @@ int main()
 
     printf("Training completed.\n");
     
-    /*double inputs[] = {0, 0};
+    double inputs[] = {0, 0};
     double *results = ProcessOutputs(network, inputs);
     printf("result=%lf\n",results[0]);
     printf("result=%lf\n",results[1]);
-*/
+
     DestroyNeuralNetwork(network);
     for (size_t i = 0; i < 4; i++){
         DestroyDatapoint(sample[i]);
         free(sample[i]);
     }
-    free(sample);
-    return 0;
+    free(sample);*/
 }
