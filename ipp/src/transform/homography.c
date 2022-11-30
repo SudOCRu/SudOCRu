5f0883ecc20c9892379005979562bbaccd917189
Sofiane <sofiane.meftah@outlook.fr>
2022-11-30 20:13:40 +0100

    feat: Add GetHomographyMatrix

--- ipp/src/transform/homography.c
+++ ipp/src/transform/homography.c
@@ -1,1 +1,45 @@
 #include "homography.h"
+
+Matrix* GetHomographyMatrix(const BBox* from, const BBox* to)
+{
+    float a_vals[8 * 8] =
+    {
+        from->x1, from->y1, 1, 0, 0, 0, -to->x1 * from->x1, -to->x1*from->y1,
+        0, 0, 0, from->x1, from->y1, 1, -to->y1 * from->x1, -to->y1*from->y1,
+
+        from->x2, from->y2, 1, 0, 0, 0, -to->x2 * from->x2, -to->x2*from->y2,
+        0, 0, 0, from->x2, from->y2, 1, -to->y2 * from->x2, -to->y2*from->y2,
+
+        from->x3, from->y3, 1, 0, 0, 0, -to->x3 * from->x3, -to->x3*from->y3,
+        0, 0, 0, from->x3, from->y3, 1, -to->y3 * from->x3, -to->y3*from->y3,
+
+        from->x4, from->y4, 1, 0, 0, 0, -to->x4 * from->x4, -to->x4*from->y4,
+        0, 0, 0, from->x4, from->y4, 1, -to->y4 * from->x4, -to->y4*from->y4,
+    };
+    float b_vals[8] = {
+        to->x1, to->y1,
+        to->x2, to->y2,
+        to->x3, to->y3,
+        to->x4, to->y4,
+    };
+    Matrix* a = NewMatrix(8, 8, a_vals);
+    Matrix* b = NewMatrix(8, 1, b_vals);
+    Matrix* at = MatTranspose(a);
+
+    // We need to compute H = (at*a)^-1 * at * b
+    //                <=> H = iv^-1 * atb
+
+    Matrix* atb = MatMultiply(at, b);
+    Matrix* iv = MatMultiply(at, a);
+    if (!MatInvert(iv))
+        return NULL;
+
+    Matrix* h = MatMultiply(iv, atb);
+
+    DestroyMatrix(iv);
+    DestroyMatrix(atb);
+    DestroyMatrix(at);
+    DestroyMatrix(b);
+    DestroyMatrix(a);
+    return h;
+}
--- ipp/src/transform/homography.h
+++ ipp/src/transform/homography.h
@@ -1,2 +1,5 @@
 #pragma once
 #include "matrix.h"
+#include "../grid_slicer/geom.h"
+
+Matrix* GetHomographyMatrix(const BBox* from, const BBox* to);
