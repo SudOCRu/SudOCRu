1ff9d33c5cc9a26b0da79cfa529551b42c2f1734
Sofiane <sofiane.meftah@outlook.fr>
2022-12-08 22:06:12 +0100

    feat: Add file chooser

--- app/src/utils.c
+++ app/src/utils.c
@@ -2,31 +2,18 @@
 
 void CopySurfaceToGdkImage(SDL_Surface *surface, GtkImage* image)
 {
-    Uint32 src_format;
-    Uint32 dst_format;
-
-    GdkPixbuf *pixbuf;
-    gboolean has_alpha;
-    int rowstride;
-    guchar *pixels;
-
-    //GtkWidget *image;
-
     // select format
-    src_format = surface->format->format;
-    has_alpha = SDL_ISPIXELFORMAT_ALPHA(src_format);
-    if (has_alpha) {
-        dst_format = SDL_PIXELFORMAT_RGBA32;
-    }
-    else {
-        dst_format = SDL_PIXELFORMAT_RGB24;
-    }
+    Uint32 src_format = surface->format->format;
+    gboolean has_alpha = SDL_ISPIXELFORMAT_ALPHA(src_format);
+
+    Uint32 dst_format = has_alpha ?
+        SDL_PIXELFORMAT_RGBA32 : SDL_PIXELFORMAT_RGB24;
 
     // create pixbuf
-    pixbuf = gdk_pixbuf_new (GDK_COLORSPACE_RGB, has_alpha, 8,
+    GdkPixbuf *pixbuf = gdk_pixbuf_new(GDK_COLORSPACE_RGB, has_alpha, 8,
                              surface->w, surface->h);
-    rowstride = gdk_pixbuf_get_rowstride (pixbuf);
-    pixels = gdk_pixbuf_get_pixels (pixbuf);
+    int rowstride = gdk_pixbuf_get_rowstride (pixbuf);
+    guchar *pixels = gdk_pixbuf_get_pixels (pixbuf);
 
     // copy pixels
     SDL_LockSurface(surface);
@@ -40,6 +27,4 @@ void CopySurfaceToGdkImage(SDL_Surface *surface, GtkImage* image)
 
     // release our reference to the pixbuf
     g_object_unref (pixbuf);
-
-    //return image;
 }
--- app/src/windows/main.c
+++ app/src/windows/main.c
@@ -3,14 +3,37 @@
 void import_file (GtkButton *button, gpointer user_data)
 {
     SudOCRu* app = user_data;
-    // TODO
+
+    GtkWindow* win = GTK_WINDOW(gtk_builder_get_object(app->ui,
+                "MainWindow"));
+    GtkWidget *dialog = gtk_file_chooser_dialog_new("Choose a file",
+            GTK_WINDOW(win), 
+            GTK_FILE_CHOOSER_ACTION_OPEN,
+            "Cancel", GTK_RESPONSE_CANCEL,
+            "Open", GTK_RESPONSE_ACCEPT, NULL);
+
+    GtkFileFilter *filter = gtk_file_filter_new();
+    gtk_file_filter_set_name(filter, "Images (png, jpg)");
+    gtk_file_filter_add_pattern(filter, "*.png");
+    gtk_file_filter_add_mime_type(filter, "image/png");
+    gtk_file_filter_add_pattern(filter, "*.jpg");
+    gtk_file_filter_add_mime_type(filter, "image/jpg");
+    gtk_file_chooser_add_filter(GTK_FILE_CHOOSER(dialog), filter);
+
+    gtk_widget_show_all(dialog);
+    gint resp = gtk_dialog_run(GTK_DIALOG(dialog));
+    if (resp == GTK_RESPONSE_OK)
+        g_print("%s\n", gtk_file_chooser_get_filename(GTK_FILE_CHOOSER(dialog)));
+    else
+        g_print("You pressed the cancel\n");
+    gtk_widget_destroy(dialog);
 }
 
 void show_about_dialog(GtkButton *button, gpointer user_data)
 {
     SudOCRu* app = user_data;
-    GtkWindow* win = GTK_WINDOW(gtk_builder_get_object(app->ui, "MainWindow"));
-    GtkWindow* dialog = GTK_WINDOW(gtk_builder_get_object(app->ui, "AboutUsPopup"));
+    GtkWindow* dialog = GTK_WINDOW(gtk_builder_get_object(app->ui,
+                "AboutUsPopup"));
 
     gtk_window_set_destroy_with_parent(dialog, TRUE);
     gtk_window_set_modal(dialog, TRUE);
@@ -26,8 +49,10 @@ gboolean hide_window(GtkWidget *widget, GdkEvent *event, gpointer data)
 
 void SetupMainWindow(SudOCRu* app)
 {
-    GtkWindow* win = GTK_WINDOW(gtk_builder_get_object(app->ui, "MainWindow"));
-    GtkWindow* dialog = GTK_WINDOW(gtk_builder_get_object(app->ui, "AboutUsPopup"));
+    GtkWindow* win = GTK_WINDOW(gtk_builder_get_object(app->ui,
+                "MainWindow"));
+    GtkWindow* dialog = GTK_WINDOW(gtk_builder_get_object(app->ui,
+                "AboutUsPopup"));
     GtkButton* load = GTK_BUTTON(gtk_builder_get_object(app->ui,
                 "LoadFileButton"));
     GtkButton* openAbout = GTK_BUTTON(gtk_builder_get_object(app->ui,
