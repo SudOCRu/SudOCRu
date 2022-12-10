ffc99ef892fb5c77f3157d10438be5564e10bd86
mehdi.el-alaoui-el-aoufoussi <mehdi.el-alaoui-el-aoufoussi@epita.fr>
2022-12-10 18:50:39 +0100

    Trained a lot more neural network
    Signed-off-by: mehdi.el-alaoui-el-aoufoussi <mehdi.el-alaoui-el-aoufoussi@epita.fr>

--- ocr/src/tester/encoder.py
+++ ocr/src/tester/encoder.py
@@ -1,15 +1,46 @@
-import sys
-import os
-import struct
-import time
+import sys,os,struct,time,random
 from subprocess import run
 from tkinter import *
-from PIL import Image,ImageTk
+from PIL import Image,ImageTk,ImageFont,ImageDraw
 
 class Tester:
     def __init__(self, paths):
-        self.i = 1
+        self.i = 0
         self.paths = paths
+
+def writeNewTrainingSet():
+    samples_base = "train/"
+    num = []
+    pixs = []
+    with open("../training", "wb") as f:
+        for i in range(10):
+            folder = os.path.join(samples_base, str(i))
+            for image_path in os.listdir(folder):
+                image_path = os.path.join(folder, image_path)
+                if os.path.isfile(image_path):
+                    num.append(i)
+                    img = Image.open(image_path)
+                    bands = img.getbands()
+                    for j in range(img.width*img.height):
+                        if bands == ('P',):
+                            p = img.getpixel((j%img.width, j//img.width))
+                            normalized = (p*255)//9
+                        else:
+                            r,g,b = img.getpixel((j%img.width, j//img.width))
+                            normalized = (r+g+b)//3
+                        pixs.append(normalized)
+        f.write((2051).to_bytes(4, byteorder='little', signed=True))
+        f.write((len(num)).to_bytes(4, byteorder='little', signed=True))
+        f.write((28).to_bytes(4, byteorder='little', signed=True))
+        f.write((28).to_bytes(4, byteorder='little', signed=True))
+        f.write(bytearray(pixs))
+
+    with open('../labels', 'wb') as f:
+        f.write((2049).to_bytes(4, byteorder='little', signed=True))
+        f.write((len(num)).to_bytes(4, byteorder='little', signed=True))
+        f.write(bytearray(num))
+    print('total images encoded:',len(num))
+
 
 def writeTrainingSet():
     samples_base = "train/"
@@ -63,6 +94,108 @@ def grayscaleImage(imagePath):
                 t = ((r+g+b)//3)
                 f.write(struct.pack('d', t))
 
+def isWhiteAtLine(line : int, img):
+    for i in range(img.width):
+        r, g, b = img.getpixel((i, line))
+        bw = (r+g+b)//3
+        if bw > 100:
+            return True
+    return False
+
+def MaxImage(f : ImageFont, text):
+    img = Image.new(mode="RGB", size=(28,28), color=(0, 0, 0))
+    drawer = ImageDraw.Draw(img)
+    size = f.size
+    l, t, r, b = 0, 0, 0, 0
+    iterations = 0
+    while (b-t) != 28:
+        size += 1
+        f = ImageFont.truetype(f.path, size)
+        l, t, r, b = drawer.textbbox((-l, -t), text, font=f)
+        iterations += 1
+    l, t, r, b = drawer.textbbox((0, 0), text, font=f)
+    width = r-l
+    drawer.text((-l+(14-width//2), -t), text, (255, 255, 255), font=f)
+    return img
+
+def SaveImageWithRotations(img : Image, j, currPath):
+    imgPath = os.path.join(currPath, "v"+str(j)+".png")
+    img.save(imgPath)
+    j+=1
+    imgPath = os.path.join(currPath, "v"+str(j)+".png")
+    img.rotate(-5).save(imgPath)
+    j+=1
+    imgPath = os.path.join(currPath, "v"+str(j)+".png")
+    img.rotate(5).save(imgPath)
+    return j+1
+
+def test(fontPaths):
+    fonts = []
+    size = 20
+    for fontPath in fontPaths:
+        fonts.append(ImageFont.truetype(fontPath, size))
+    
+    for i in range(10):
+        currPath = "train/"+str(i)+"/"
+        if not os.path.exists(currPath):
+            os.mkdir(currPath)
+        j = 0
+        for font in fonts:
+            img = MaxImage(font, str(i))
+            j = SaveImageWithRotations(img, j, currPath)
+            new = img.transform(img.size, Image.AFFINE, (1, 0, 0, 0, 1, -3))
+            j = SaveImageWithRotations(new, j, currPath)
+            new = img.transform(img.size, Image.AFFINE, (1, 0, 0, 0, 1, 3))
+            j = SaveImageWithRotations(new, j, currPath)
+
+def generateImgs(fonts):
+    rotId = 0
+    for offset in range(-5, 6, 5):
+        for rots in range(-5, 6, 5):
+            length = len(fonts)
+            for id in range(len(fonts)):
+                size = 20
+                img = Image.open("base.png")
+                font = ImageFont.truetype(fonts[id], size)
+                I1 = ImageDraw.Draw(img)
+                y = 5
+                I1.text((4, y), "0", font=font, fill=(255,255,255))
+                for i in range (5):
+                    while not isWhiteAtLine(0, img):
+                        y-=1
+                        img = Image.open("base.png")
+                        I1 = ImageDraw.Draw(img)
+                        I1.text((4, y), "0", font=font, fill=(255,255,255))
+                    while not isWhiteAtLine(img.height-1, img):
+                        img = Image.open("base.png")
+                        I1 = ImageDraw.Draw(img)
+                        size += 1
+                        font = ImageFont.truetype(fonts[id], size)
+                        I1.text((4, y), "0", font=font, fill=(255,255,255))
+                
+                if not os.path.exists("train/sample"+str(rotId*length+id)+"/"):
+                    os.mkdir("train/sample"+str(rotId*length+id))
+                for i in range(10):
+                    img = Image.open("base.png")
+                    I1 = ImageDraw.Draw(img)
+                    _, _, w, h = I1.textbbox((0, 0), str(i), font=font)
+                    I1.text(((28-w)/2, y), str(i), font=font, fill=(255,255,255))
+                    pixels = img.load()
+                    for j in range(120):
+                        pixel = random.randrange(0, 28*28-1)
+                        pixels[pixel%28, pixel//28] = (0, 0, 0)
+                    """if rots == 1:
+                        img.rotate(5).save("train/sample"+str(rots*length+id)+"/"+str(i)+".png")
+                    elif rots == 2:"""
+                    img.rotate(rots).save("train/sample"+str(rotId*length+id)+"/"+str(i)+".png")
+                    print("rotation: ",rots,"degs")
+                    """else:
+                        img.save("train/sample"+str(rots*length+id)+"/"+str(i)+".png")"""
+            rotId+=1
+
+def RotateImg(img : Image, deg):
+    img.rotate(-deg)
+
 st = time.time()
 
 if len(sys.argv) < 2:
@@ -70,12 +203,16 @@ if len(sys.argv) < 2:
     print('test <path>: path to the image to encode and test')
     print('train: encode samples to learn')
     print('suit <path>: path to the folder containing images')
+    print('generator: generate image samples for training')
     exit(1)
 
 index = 0
 
-def nextImage(t : Tester, label : Label, label2 : Label, labelpath : Label, dirs):
-    if t.i < len(dirs):
+def nextImage(t : Tester, label : Label, label2 : Label, labelpath : Label, dirs, offset):
+    if t.i == 0 and offset < 0:
+        labelpath["text"] = "Beginning of the folder"
+    elif t.i < len(dirs):
+        t.i+=offset
         grayscaleImage(os.path.join(t.paths, dirs[t.i]))
         os.chdir("../")
         v = run(['./tests', 'eval'])
@@ -85,10 +222,9 @@ def nextImage(t : Tester, label : Label, label2 : Label, labelpath : Label, dirs
         labelpath["text"]=dirs[t.i]
         label.configure(image=img)
         label.image = img
-        t.i+=1
+        print("offset=",offset," t.i=",t.i)
     else:
         labelpath["text"]="All images readed"
-
 
 if sys.argv[1] == 'test' and len(sys.argv) == 3:
     grayscaleImage(sys.argv[2])
@@ -97,7 +233,8 @@ if sys.argv[1] == 'test' and len(sys.argv) == 3:
     print("result = ",str(v.returncode))
     os.chdir("tester")
 elif sys.argv[1] == 'train':
-    writeTrainingSet()
+    writeNewTrainingSet()
+    #writeTrainingSet()
 elif sys.argv[1] == 'suit' and len(sys.argv) == 3:
     dirs = os.listdir(sys.argv[2])
 
@@ -122,9 +259,19 @@ elif sys.argv[1] == 'suit' and len(sys.argv) == 3:
     label2.pack()
     a = Tester(sys.argv[2])
     
-    next = Button(text="Next",command=lambda: nextImage(a, label, label2, labelpath, dirs))
+    next = Button(text="Next",command=lambda: nextImage(a, label, label2, labelpath, dirs, 1))
     next.pack()
+    back = Button(text="Back",command=lambda: nextImage(a, label, label2, labelpath, dirs, -1))
+    back.pack()
     window.mainloop()
+elif sys.argv[1] == 'generator':
+    fonts = []
+    font_path = "fonts/"
+    for path in os.listdir(font_path):
+        if os.path.isfile(os.path.join(font_path, path)):
+            fonts.append(font_path+path)
+    #generateImgs(fonts)
+    test(fonts)
 
 et = time.time()
 print('Executions time:',(et-st)*1000,'ms')
